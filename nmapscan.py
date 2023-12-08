f4a561621899a56d36c86ce7619a5fef29c72ad7
Nathan Abreu <74798133+obliviongx@users.noreply.github.com>
2023-12-08 13:15:49 +0100

    Update nmapscan.py

--- nmapscan.py
+++ nmapscan.py
@@ -1,74 +1,45 @@
-#!/var/ossec/framework/python/bin/python3
-# Copyright (C) 2015-2023, Wazuh Inc.
-
+################################
+### Python Script to Run Network Scans and append results to Wazuh Active Responses Log
+### Requirements:
+###     NMAP installed in Agent
+###     python-nmap (https://pypi.org/project/python-nmap/)
+### Replace the Array "subnets" with the subnets to scan from this agent.
+### Do NOT include subnets with a network firewall in the path of the agent and the subnet.
+################################
 import nmap
 import time
 import json
-import platform
-
-# The function to perform network scan on a host endpoint
-def scan_subnet(subnet):
-    nm = nmap.PortScanner()
+nm = nmap.PortScanner()
+#Add subnets to scan to the Subnets Array
+subnets=['10.105.9.0/24']
+for subnet in subnets:
+    json_output={}
     nm.scan(subnet)
-    results = []
-
     for host in nm.all_hosts():
+        json_output['nmap_host']=host
         for proto in nm[host].all_protocols():
             if proto not in ["tcp", "udp"]:
                 continue
-
+            json_output['nmap_protocol']=proto
             lport = list(nm[host][proto].keys())
             lport.sort()
-# Iterate over each port for the current host and protocol
             for port in lport:
                 hostname = ""
-                json_output = {
-                    'nmap_host': host,
-                    'nmap_protocol': proto,
-                    'nmap_port': port,
-                    'nmap_hostname': "",
-                    'nmap_hostname_type': "",
-                    'nmap_port_name': "",
-                    'nmap_port_state': "",
-                    'nmap_port_service': ""
-                }
-# Get the first hostname and it’s type
-                if nm[host]["hostnames"]:
-                    hostname = nm[host]["hostnames"][0]["name"]
-                    hostname_type = nm[host]["hostnames"][0]["type"]
-                    json_output['nmap_hostname'] = hostname
-                    json_output['nmap_hostname_type'] = hostname_type
-# Get the port name if available
-                if 'name' in nm[host][proto][port]:
-                    json_output['nmap_port_name'] = nm[host][proto][port]['name']
-# Get the port state if available
-                if 'state' in nm[host][proto][port]:
-                    json_output['nmap_port_state'] = nm[host][proto][port]['state']
-# Get the port service version if available
-                if 'product' in nm[host][proto][port] and 'version' in nm[host][proto][port]:
-                    service = nm[host][proto][port]['product'] + " " + nm[host][proto][port]['version']
-                    json_output['nmap_port_service'] = service
-
-                results.append(json_output)
-    return results
-
-# The function to append the scan results to the active response log file
-def append_to_log(results, log_file):
-    with open(log_file, "a") as active_response_log:
-        for result in results:
-            active_response_log.write(json.dumps(result))
-            active_response_log.write("\n")
-# Specify the address(es) to scan
-subnets = ['127.0.0.1']
-# path of the log file
-if platform.system() == 'Windows':
-    log_file = "C:\\Program Files (x86)\\ossec-agent\\active-response\\active-responses.log"
-elif platform.system() == 'Linux':
-    log_file = "/var/ossec/logs/active-responses.log"
-else:
-    log_file = "/Library/Ossec/logs/active-responses.log"
-
-for subnet in subnets:
-    results = scan_subnet(subnet)
-    append_to_log(results, log_file)
-    time.sleep(2)
+                json_output['nmap_port']=port
+                for h in nm[host]["hostnames"]:
+                    hostname = h["name"]
+                    json_output['nmap_hostname']=hostname
+                    hostname_type = h["type"]
+                    json_output['nmap_hostname_type']=hostname_type
+                    json_output['nmap_port_name']=nm[host][proto][port]["name"]
+                    json_output['nmap_port_state']=nm[host][proto][port]["state"]
+                    json_output['nmap_port_product']=nm[host][proto][port]["product"]
+                    json_output['nmap_port_extrainfo']=nm[host][proto][port]["extrainfo"]
+                    json_output['nmap_port_reason']=nm[host][proto][port]["reason"]
+                    json_output['nmap_port_version']=nm[host][proto][port]["version"]
+                    json_output['nmap_port_conf']=nm[host][proto][port]["conf"]
+                    json_output['nmap_port_cpe']=nm[host][proto][port]["cpe"]
+                    with open("/var/ossec/logs/active-responses.log", "a") as active_response_log:
+                        active_response_log.write(json.dumps(json_output))
+                        active_response_log.write("\n")
+                time.sleep(2)
